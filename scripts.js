40d2d7261bd830ed907b6c813763d0b9d062e5c8
RingK27 <111756643+RingK27@users.noreply.github.com>
2022-09-17 16:32:12 +0200

    Added basic functionality

--- scripts.js
+++ scripts.js
@@ -1,12 +1,42 @@
 let squares = document.getElementsByClassName('square');
 
-for(let i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('click', function() {
-        console.log(this.id);
-    });
-}
+const gameController = (() => {
+    let turn = 0; 
 
-const gameBoard = () => {
-    let cells = [9];
-}
+    for(let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener('click', function() {  
+            if (gameBoard.board[this.id] == '') {
+                if (turn == 0 || (turn % 2) == 0) {
+                    gameBoard.board[this.id] = "<img src='.\\images\\circle.png'>";
+                } else {
+                    gameBoard.board[this.id] = "<img src='.\\images\\cross.png'>";
+                }        
+                gameBoard.drawBoard(gameBoard.board);
+                turn += 1;
+            } else {
+                return;
+            } 
+            if (turn > 3) {
+                checkWin();
+            }
+        });
+    };
+    const checkWin = () => console.log('no winner');    
+})();
+
+const gameBoard = (() => {       
+    let board = ['','','','','','','','',''];
+    const drawBoard = ([]) => {
+        for (let i = 0; i < 9; i++) {
+            squares[i].innerHTML = board[i];
+        }
+    } 
+    const clearBoard = () => console.log('board cleared');
+
+    return {        
+        board,
+        drawBoard,
+        clearBoard,               
+    };
+})();
 
